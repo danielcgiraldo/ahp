50f0c76890d09b3766ae4823463fb98847380017
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-11-30 18:08:59 -0500

    remove adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind(), react()],
-  adapter: vercel()
+  integrations: [tailwind(), react()]
 });
